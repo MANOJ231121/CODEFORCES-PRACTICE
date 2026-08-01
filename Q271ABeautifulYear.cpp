9402423da313bcaa7ec8d5b3c67afdd13af7f441
Manoj <bhawanajoshi979@gmail.com>
2026-08-02 01:50:42 +0530

    1st july question

---
+++ Q271ABeautifulYear.cpp
@@ -0,0 +1,35 @@
+// It seems like the year of 2013 came only yesterday. Do you know a curious fact? The year of 2013 is the first year after the old 1987 with only distinct digits.
+
+// Now you are suggested to solve the following problem: given a year number, find the minimum year number which is strictly larger than the given one and has only distinct digits.
+
+// Input
+// The single line contains integer y (1000 ≤ y ≤ 9000) — the year number.
+
+// Output
+// Print a single integer — the minimum year number that is strictly larger than y and all it's digits are distinct. It is guaranteed that the answer exists.
+
+// 1987
+// 2013
+#include<string>
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >>n;
+    string s;
+    while(true){
+        n++;
+        s=to_string(n);
+        if(s[0] != s[1] &&
+            s[0] != s[2] &&
+            s[0] != s[3] &&
+            s[1] != s[2] &&
+            s[1] != s[3] &&
+            s[2] != s[3]){
+                cout<<n<<endl;
+                break;
+        }
+    }
+        
+    return 0;
+}
