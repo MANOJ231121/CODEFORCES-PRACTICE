d3ce7c8c43919281e70dee4ba01ea89ea23940dd
Manoj <bhawanajoshi979@gmail.com>
2026-08-18 23:12:35 +0530

    18th july question

---
+++ Q133HQ.cpp
@@ -0,0 +1,16 @@
+#include<string>
+#include<iostream>
+using namespace std;
+int main(){
+    string n;
+    cin>>n;
+    for (int i = 0; i < n.size(); i++)
+    {
+        if(n[i] == 'H' || n[i]=='Q' || n[i]=='9' ){
+            cout<<"YES"<<endl;
+            return 0;
+            }
+    }
+    cout<<"NO"<<endl;
+    return 0;
+}
